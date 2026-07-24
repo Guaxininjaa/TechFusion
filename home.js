0fa989d0af09e3e9edabe77ad47f3121461c4e50
BEATRIZ LUIZA POLIZEL <beatriz_polizel@estudante.sesisenai.org.br>
2026-07-23 22:13:35 -0300

    alterações

--- home.js
+++ home.js
@@ -3,18 +3,51 @@ const senha = document.querySelector("#senha");
 
 eye.addEventListener("click",()=>{
 
-if(senha.type==="password"){
+    if(senha.type==="password"){
 
-senha.type="text";
+        senha.type="text";
 
-eye.classList.replace("fa-eye","fa-eye-slash");
+        eye.classList.replace("fa-eye","fa-eye-slash");
 
-}else{
+    }else{
 
-senha.type="password";
+        senha.type="password";
 
-eye.classList.replace("fa-eye-slash","fa-eye");
+        eye.classList.replace("fa-eye-slash","fa-eye");
 
-}
+    }
+
+});
+
+const login = document.querySelector(".login");
+
+login.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    const email = document.querySelector('input[type="email"]').value;
+    const senhaDigitada = document.querySelector("#senha").value;
+
+    const empresa = JSON.parse(localStorage.getItem("empresa"));
+
+    if(!empresa){
+
+        alert("Nenhuma empresa cadastrada.");
+
+        return;
+
+    }
+
+    if(email === empresa.email && senhaDigitada === empresa.senha){
+
+        localStorage.setItem("logado","true");
+
+        window.location.href = "dashboard.html";
+
+    }else{
+
+        alert("E-mail ou senha incorretos.");
+
+    }
 
 });
